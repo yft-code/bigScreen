e70f9584833514abc55b734500ec1e448e7e54f5
LAPTOP-AG4T4L0N\yft123 <2997978921@qq.com>
2023-02-10 15:16:24 +0800

    大屏报表方案二使用css的scale（好像还不对，后期修改）

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,18 +1,84 @@
 <template>
-  <div class="indexContainer">
-      <largeScreen/>
+   <div
+    :style="{
+      'transformOrigin':'center top',
+      'transform':`scale(${rate},${rate})`,
+      '-webkit-transform':`scale(${rate},${rate})`,
+      '-moz-transform':`scale(${rate},${rate})`,
+      '-o-transform':`scale(${rate},${rate})`,
+      '-ms-transform':`scale(${rate},${rate})`
+    }"
+    >
+    <div class="title">
+        <div class="left">left</div>
+        <div class="center">center</div>
+        <div class="right">right</div>
+    </div>
   </div>
 </template>
 <script>
-import largeScreen from '@/components/largeScreen/index.vue'
 export default {
-  components: {largeScreen},
-  name: "Login"
-};
+  name: "Login",
+  data(){
+return{
+	rate:1
+   }
+},
+mounted() {
+    this.resize_window()
+    window.addEventListener('resize', () => {
+      this.resize_window()
+    })
+  },
+destroyed() {
+  window.removeEventListener('resize', () => {
+    this.resize_window()
+  })
+},
+methods: {
+	//在这里发送页面的请求获取大屏展示的数据
+    async initialize() {
+      try {
+        this.loading = true
+        this.$emit('pageLoading', true)
+      } catch (err) {
+        console.log(err)
+      } finally {
+        setTimeout(() => {
+          this.loading = false
+          this.$emit('pageLoading', false)
+        }, 3000)
+      }
+    },
+    resize_window() {
+      const heightRatio = Number(document.documentElement.clientHeight / 1080)
+      const widthRatio = Number(document.documentElement.clientWidth / 1920)
+      var ratio= document.documentElement.clientWidth /document.documentElement.clientHeight
+      if (ratio< 1920 / 1080) {
+        this.rate= widthRatio 
+      } else {
+        this.rate= heightRatio 
+      }
+    }
+  }
+}
+
 </script>
 <style lang="scss" scoped>
-.indexContainer{
+.title{
+  display: flex;
+  // width: 100%;
   height: 100vh;
-  /* background-color: cadetblue; */
+  
+}
+.right,.left{
+ height: 100%;
+ width: 100px;
+ background-color: aqua;
+}
+.center{
+  flex: 1;
+  height: 100%;
+  background-color: blanchedalmond;
 }
 </style>
